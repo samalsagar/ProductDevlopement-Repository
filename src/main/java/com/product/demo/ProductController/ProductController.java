d400de886fd87895535ce5402f7da00e87da02f3
preetinanda <preetinanda2k@gmail.com>
2023-06-21 23:16:14 +0530

    msg

--- src/main/java/com/product/demo/ProductController/ProductController.java
+++ src/main/java/com/product/demo/ProductController/ProductController.java
@@ -1,24 +1,28 @@
 package com.product.demo.ProductController;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.product.demo.ProductService.ProductServiceImpl;
 import com.product.demo.dto.ProductPojo;
+import com.product.demo.entity.ProductEntity;
 
-@Controller
+@RestController
 public class ProductController {
 
 	@Autowired
 	private ProductServiceImpl productServiceImpl;
-       
+
 	@GetMapping("/loadFrom")
 	public String saveData(Model model) {
 		System.out.println("Load Frm");
@@ -26,19 +30,46 @@ public class ProductController {
 		model.addAttribute("productPojo", object);
 		return "home";
 	}
-	
+
 	@PostMapping("/saveProduct")
-	public String saveProduct(@RequestBody ProductPojo productPojo, @ModelAttribute ProductPojo productPojo2) {
+	public String saveProduct(@RequestBody ProductPojo productPojo) {
 		System.out.println(" ---->" + productPojo);
 		boolean addProduct = productServiceImpl.addProduct(productPojo);
-		if(addProduct) {
+		if (addProduct) {
 			System.out.println("Data Saved successfully");
 		} else {
 			System.out.println("Data Not Saved");
 		}
-		return "null";
+		return "Record saved successfully";
 	}
 	
+	@GetMapping("/getAllData")
+	public List<ProductPojo> getAllData(Model model) {//Break point is there 
+		
+		List<ProductPojo> allProduct = productServiceImpl.getAllProduct();
+		System.out.println("All Product-->" + allProduct);
+
+		
+		return allProduct;//AND here another break point
+	}
 	
+	@GetMapping("/getone/{id}")
+	public ProductPojo getProductById(@PathVariable("id") int id,Model Model)
+	{
+		return productServiceImpl.getProductById(id);
+	}
 	
+	@PutMapping("/editproduct/{id}")
+	public String editProduct(@PathVariable("id")int id,@RequestBody ProductEntity productEntity)
+	{
+		productServiceImpl.editProduct(id, productEntity);
+		return "Record Updated sucessfully";
+	}
+
+	@DeleteMapping("/deleteprod/{id}")
+	public String deleteProductById(@PathVariable("id") int id)
+	{
+		
+		return productServiceImpl.deleteProductById(id);
+	}
 }
--- src/main/java/com/product/demo/ProductService/ProductService.java
+++ src/main/java/com/product/demo/ProductService/ProductService.java
@@ -1,9 +1,20 @@
 package com.product.demo.ProductService;
 
+import java.util.List;
+
 import com.product.demo.dto.ProductPojo;
+import com.product.demo.entity.ProductEntity;
 
 
 public interface ProductService {
 	
 	public boolean addProduct(ProductPojo prodcutPojo);
+	
+	public List<ProductPojo> getAllProduct();
+	
+	public ProductPojo getProductById(int id);
+	
+	public String deleteProductById(int id);
+	
+	public Boolean editProduct(int id, ProductEntity productEntity);
 }
